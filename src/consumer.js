52de9f230d91f717ee98074fb5f72820b8746940
Walmyr <wlsf82@gmail.com>
2020-08-29 02:56:32 +0200

    Implement consumer test
    As part of the course of contract testing from TAU, by Rafaela
    Azevedo.

---
+++ src/consumer.js
@@ -0,0 +1,35 @@
+const axios = require('axios')
+const express = require("express")
+const server = express()
+const getApiEndpoint = "http://localhost:8081"
+
+const getClients = async () => {
+  const res = await axios
+    .get(`${getApiEndpoint}/clients`)
+    .then((res) =>  res)
+    .catch((err) => err.res)
+  return res
+}
+
+const getClient = async (id) => {
+  const res = await axios
+    .get(`${getApiEndpoint}/clients/${id}`)
+    .then((res) => res)
+    .catch((err) => err.res)
+  return res
+}
+
+const postClient = async (body) => {
+  const res = await axios
+    .post(`${getApiEndpoint}/clients`, body, {'Content-Type': 'application/json;charset=utf-8'})
+    .then((res) => res)
+    .catch((err) => err.res)
+  return res
+}
+
+module.exports = {
+  server,
+  getClients,
+  postClient,
+  getClient,
+};
